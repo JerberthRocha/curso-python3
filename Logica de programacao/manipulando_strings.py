439cacefb12e072f1b6e22979ec4079764a35d0f
Jerberth Rocha <jerberthrs@gmail.com>
2022-05-11 21:49:18 -0400

    commit inicial

---
+++ Logica de programacao/manipulando_strings.py
@@ -0,0 +1,8 @@
+# https://docs.python.org/3/library/functions.html (funções built-in)
+# https://docs.python.org/3/library/stdtypes.html (Tipos built-in)
+texto = 'python s2'
+print(texto[2])
+print(texto[7])
+print(texto[2:6])
+# continue - pula para a próxima iteração do laço
+# for - sai do laço
