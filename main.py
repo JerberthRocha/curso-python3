595adf43096743766cc2ea1056f90c01d20e03df
Jerberth Rocha <jerberthrs@gmail.com>
2022-05-26 19:58:23 -0400

    Assosiação em Python

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from Orientacao_a_objetos.classe_pessoa import Pessoa
-from Orientacao_a_objetos.classe_produto import Produto
+# from Orientacao_a_objetos.classe_pessoa import Pessoa
+# from Orientacao_a_objetos.classe_produto import Produto
 
-pessoa1 = Pessoa('Maria', 29)
-pessoa2 = Pessoa('José', 32)
+# pessoa1 = Pessoa('Maria', 29)
+# pessoa2 = Pessoa('José', 32)
 
 # pessoa1.comer('pizza')
 # pessoa1.falar('POO')
@@ -22,15 +22,39 @@ pessoa2 = Pessoa('José', 32)
 # print(pessoa1.nome, pessoa1.idade)
 # pessoa1.get_ano_nascimento()
 
+
 # @PROPERTY - GETTERS e SETTERS 
-produto1 = Produto('CAMISETA', 100)
-produto1.desconto(10)
-print(produto1.nome, produto1.preco)
+# produto1 = Produto('CAMISETA', 100)
+# produto1.desconto(10)
+# print(produto1.nome, produto1.preco)
 
-produto2 = Produto('Calça', 'R$250')
-produto2.desconto(20)
-print(produto2.nome, produto2.preco)
+# produto2 = Produto('Calça', 'R$250')
+# produto2.desconto(20)
+# print(produto2.nome, produto2.preco)
 
-produto3 = Produto('Meias', 20)
-produto3.desconto(10)
-print(produto3.preco)
+# produto3 = Produto('Meias', 20)
+# produto3.desconto(10)
+# print(produto3.preco)
+
+
+# ASSOCIAÇÃO
+from Orientacao_a_objetos.associacao import Escritor, MaquinaDeEscrever, Caneta
+
+
+escritor1 = Escritor('Clarisse')
+escritor2 = Escritor('Maria')
+
+caneta1 = Caneta('Bic')
+maquina1 = MaquinaDeEscrever()
+
+caneta1.ferramenta = caneta1
+caneta1.ferramenta.escrever()
+
+escritor2.ferramenta = maquina1
+escritor2.ferramenta.escrever()
+
+# caneta1 = Caneta('Bic')
+# print(caneta1.marca)
+
+# maquina1 = MaquinaDeEscrever()
+# maquina1.escrever()
