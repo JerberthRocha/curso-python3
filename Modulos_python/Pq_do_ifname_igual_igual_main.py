3b8c6e402fb346d8f509ca91a45c524dae174bd2
Jerberth Rocha <jerberthrs@gmail.com>
2022-06-14 13:18:06 -0400

    Pq usar o if name igual igual a main

---
+++ Modulos_python/Pq_do_ifname_igual_igual_main.py
@@ -0,0 +1,5 @@
+# if __name__ == "__main__"
+
+from modulo import soma
+
+print(soma(20,2))
---
+++ Modulos_python/modulo.py
@@ -0,0 +1,15 @@
+def soma(x: float, y:float) -> float:
+    return x + y
+
+def main() -> None:
+    print(__name__)
+    print(soma(20,30))
+    print(soma(20,10))
+
+if __name__ == '__main__':
+    main()
+
+# if __name__ == '__main__':
+#     print(__name__)
+#     print(soma(20,30))
+#     print(soma(20,10))
