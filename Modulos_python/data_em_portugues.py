80390aa3dffcfd9471419182b58795156b333842
Jerberth Rocha <jerberthrs@gmail.com>
2022-06-14 20:28:00 -0400

    Data formatada em português, último dia do mês, último dia do mês em ano bissexto

--- Modulos_python/data_em_portugues.py
+++ Modulos_python/data_em_portugues.py
@@ -46,10 +46,10 @@ print(monthrange(2022, 1))
 # from datetime import datetime
 # from calendar import monthrange
  
-# ultimos_dias_de_meses_do_ano_atual = [
+# últimos_dias_de_meses_do_ano_atual = [
 #     monthrange(datetime.now().year, mes)[1] for mes in range(1, 13)
 # ]
-# print(ultimos_dias_de_meses_do_ano_atual)
+# print(últimos_dias_de_meses_do_ano_atual)
 # # Saída: [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 # # Observação: meu ano atual é 2020 neste momento
 # Isso deve solucionar os problemas do ano bissexto.
