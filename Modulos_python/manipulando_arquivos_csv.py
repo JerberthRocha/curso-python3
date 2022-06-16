7d1c7eaddb0466f8c3f4645f7f6cec026a16adda
JerberthR-Ubuntu <jerberthrs@gmail.com>
2022-06-15 21:54:41 -0400

    Manipulando arquivo CSV com modulo CSV em Python

--- Modulos_python/manipulando_arquivos_csv.py
+++ Modulos_python/manipulando_arquivos_csv.py
@@ -6,45 +6,45 @@ DADOS, CLIENTES DE E-MAIL, ETC...
 
 import csv
 
-# with open('dados_clientes.csv', 'r') as arquivo:
-#     dados = csv.reader(arquivo)
-#     next(dados) # PULA A PRIMEIRA LINHA POIS O ARQUIVO É UM ITERADOR
+with open('clientes.csv', 'r') as arquivo:
+    dados = csv.reader(arquivo)
+    next(dados) # PULA A PRIMEIRA LINHA POIS O ARQUIVO É UM ITERADOR
 
-#     for dado in dados:
-#         print(dado)
+    for dado in dados:
+        print(dado)
 
-# with open('clientes.csv', 'r') as arquivo:
-#     dados = csv.DictReader(arquivo)
-#     next(dados) # TIRA A PRIMEIRA LINHA POIS O ARQUIVO É UM ITERADOR
+with open('clientes.csv', 'r') as arquivo:
+    dados = csv.DictReader(arquivo)
+    next(dados) # TIRA A PRIMEIRA LINHA POIS O ARQUIVO É UM ITERADOR
 
-#     for dado in dados:
-#         print(dado['nome'], dado['sobrenome'], dado['E-mail'], dado['Telefone'])
+    for dado in dados:
+        print(dado['Nome'], dado['Sobrenome'], dado['E-mail'], dado['Telefone'])
 
-with open('clientes2.csv', 'w') as arquivo:
-    escreve = csv.writer(
-        arquivo, 
-        delimiter=',',
-        quotechar='"',
-        quoting=csv.QUOTE_ALL
-    )
+# with open('clientes2.csv', 'w') as arquivo:
+#     escreve = csv.writer(
+#         arquivo, 
+#         delimiter=',',
+#         quotechar='"',
+#         quoting=csv.QUOTE_ALL
+#     )
 
-    # chaves = dados[0].keys()
-    # chaves = list(chaves)
-    # escreve.writerow(
-    #     [
-    #         chaves[0],
-    #         chaves[1],
-    #         chaves[2],
-    #         chaves[3]
-    #     ]
-    # )
+#     # chaves = dados[0].keys()
+#     # chaves = list(chaves)
+#     # escreve.writerow(
+#     #     [
+#     #         chaves[0],
+#     #         chaves[1],
+#     #         chaves[2],
+#     #         chaves[3]
+#     #     ]
+#     # )
 
-    # for dado in dados:
-    #     escreve.writerow(
-    #         [
-    #             dado['Nome'],
-    #             dado['Sobrenome'],
-    #             dado['E-mail']
-    #             dado['Telefone']
-    #         ]
-    #     )
+#     # for dado in dados:
+#     #     escreve.writerow(
+#     #         [
+#     #             dado['Nome'],
+#     #             dado['Sobrenome'],
+#     #             dado['E-mail']
+#     #             dado['Telefone']
+#     #         ]
+#     #     )
