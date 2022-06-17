a25175754aaa5cc58852be864deec93db3d37427
JerberthR-Ubuntu <jerberthrs@gmail.com>
2022-06-16 22:08:25 -0400

    Usando Selenium para automatizar tarefas

--- Modulos_python/automatizando_tarefas.py
+++ Modulos_python/automatizando_tarefas.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 from time import sleep
 
@@ -8,16 +9,17 @@ chrome.get('https://github.com/')
 
 
 try:
-    btn_sign_up = chrome.find_element_by_link_text('Sign up')
+    btn_sign_up = chrome.find_element(By.LINK_TEXT, 'Sign up')
     btn_sign_up.click()
     sleep(2)
-    input_email = chrome.find_element_by_id('email')
+    input_email = chrome.find_element(By.ID, 'email') 
     input_email.send_keys('email@email.com.br')
     sleep(2)
-    btn_continue = chrome.find_element_by_css_selector('button')
+    btn_continue = chrome.find_element(By.CSS_SELECTOR, 'button')
     btn_continue.click()
     sleep(2)
-    input_senha = chrome.find_element_by_id('password')
+    # input_senha = chrome.find_element_by_id('password') # SINTAXE ANTIGA
+    input_senha = chrome.find_element(By.ID, 'password') # SINTAXE NOVA
     input_senha.send_keys('@Corenam23')
 except Exception as e:
     print('Não foi possivel clicar em Sign up. Erro:', e)
